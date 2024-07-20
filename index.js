7ef8808c9029215de7fb88f52ec21bb841502a2f
[Uthman] <maneraydevs@gmail.com>
2024-07-20 23:17:56 +0300

    final

---
+++ index.js
@@ -0,0 +1,21 @@
+
+/*
+// Get the image element
+const img = document.getElementById('paylink');
+
+// Add an event listener to the image
+img.addEventListener('click', function() {
+  // Open the link when the image is clicked
+  window.open('https://web.mypayd.app/maneray', '_blank');
+});
+*/
+
+
+// Get the div element
+const productCard = document.querySelector('.product-card');
+
+// Add an event listener to the div
+productCard.addEventListener('click', function() {
+  // Open the link when the div is clicked
+  window.open('https://web.mypayd.app/maneray', '_blank');
+});
